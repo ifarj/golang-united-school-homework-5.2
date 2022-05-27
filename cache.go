9b3143fe0258e59b74866c940fdd742bd1ad8cf3
Berik Akhmed <a.berik@bsl.dev>
2022-05-28 01:49:39 +0600

    implement cache

--- cache.go
+++ cache.go
@@ -3,21 +3,50 @@ package cache
 import "time"
 
 type Cache struct {
+	CacheMap map[string]ValueWithDeadline
 }
 
+type ValueWithDeadline struct {
+	Value string
+	Deadline *time.Time
+}
 func NewCache() Cache {
-	return Cache{}
+	cacheMap := make(map[string]ValueWithDeadline)
+	return Cache{CacheMap: cacheMap}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	result, ok := c.CacheMap[key]
+	if !ok {
+		return "", false
+	}
+	if result.Deadline != nil &&  result.Deadline.Before(time.Now()) {
+		delete(c.CacheMap, key)
+		return "", false
+	}
+	return result.Value, true
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.CacheMap[key] = ValueWithDeadline{
+		Value:    value,
+	}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	keys := make([]string, 0, len(c.CacheMap))
+	for k, v := range c.CacheMap {
+		if v.Deadline != nil && v.Deadline.Before(time.Now()) {
+			continue
+		}
+			keys = append(keys, k)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.CacheMap[key] = ValueWithDeadline{
+		Value:    value,
+		Deadline: &deadline,
+	}
 }
